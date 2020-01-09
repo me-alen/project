34f579364ab304d540e7075018678b76c14737c7
me-alen <alenofficial10@gmail.com>
2020-01-09 16:54:24 +0530

    initial commit

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("logged");
+import axios from "axios";
+
+async function fetchData() {
+  const response = await axios({
+    method: "fetch",
+    url: "http://10.154.4.174/players"
+    // data: {
+    //   firstName: "Finn",
+    //   lastName: "Williams"
+    // }
+  });
+  console.log(response);
+}
+
+fetchData();
