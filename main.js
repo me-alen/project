0c1d0a4fd66c709cea007000e9c5a951d51e2dd6
me-alen <alenofficial10@gmail.com>
2020-01-09 18:52:58 +0530

    get and post completed

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
+const baseEndPoint = "http://10.154.4.174/players";
 import axios from "axios";
+const userData = {
+  username: "lssd",
+  password: "lssd",
+  email: "lssd@gmail.com"
+};
 
 async function fetchData() {
-  const response = await axios({
-    method: "fetch",
-    url: "http://10.154.4.174/players"
-    // data: {
-    //   firstName: "Finn",
-    //   lastName: "Williams"
-    // }
+  return await axios({
+    url: baseEndPoint
   });
-  console.log(response);
 }
 
-fetchData();
+async function postData() {
+  const { data } = await fetchData();
+  console.log(data);
+
+  const posting = await axios({
+    method: "post",
+    url: "http://10.154.4.174/players",
+    data: JSON.stringify(userData),
+    headers: { "Content-Type": "application/json" }
+  });
+  console.log(posting);
+}
+postData();
